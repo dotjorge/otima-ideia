246eeb2dfcc587ada265e3bb9438579630dd601c
jorgeluez <jorgeluiz_dj2010@hotmail.com>
2021-04-07 14:16:04 -0300

    Tag <title>

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<title>Ótima Ideia</title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
